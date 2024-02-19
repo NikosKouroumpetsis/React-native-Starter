478763885ca2e86eac055b5ce1605cd164da4c5a
NikosKouroumpetsis <kourou67s@gmail.com>
2024-02-19 08:01:26 +0200

    Add configs and some empty screens

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   preset: 'react-native',
-};
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/navigation/**/*.tsx',
+    '!src/styles/**/*.ts',
+    '!src/types/**/*.d.ts'
+  ],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  modulePaths: ['<rootDir>/src/', '<rootDir>/.jest']
+}
